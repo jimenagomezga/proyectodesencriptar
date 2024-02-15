eb8d94dfda441fd0839041d8449407dcf51f8db5
jimenagomezga <jimenagomezga@gmail.com>
2024-02-14 20:40:02 -0500

    Actualizacion del proyecto 2

--- app.js
+++ app.js
@@ -36,34 +36,6 @@ function mensajeADesencriptar() {
   return;
 }
 
-// function copiarMensaje() {
-//   let mensajeACopiar = document.getElementById("title_text2");
-//   let button = document.querySelector("btn_copiar")
-
-//   mensajeACopiar.select()
-//   document.execCommand("copy")
-//   button.innertText = "Copiado"
-//   console.log(mensajeACopiar)
-// }
-
-
-// mensajeACopiar = document.getElementById("title_text2").innerHTML;
-// const copiarContenido = async () => {
-//   try {
-//     await navigator.clipboard.writeText(mensajeACopiar);
-//     console.log('Contenido copiado al portapapeles');
-//   } catch (err) {
-//     console.error('Error al copiar: ', err);
-//   }
-// }
-
-
-// (".btn_copiar").addEventListener("click", async () => {
-//   await navigator.clipboard.writeText(section_2text.value)
-//   alert('Copiado');
-// })
-
-
 document.querySelector(".btn_copiar").addEventListener("click", function () {
   let copiarTexto = document.querySelector("h3").innerHTML;
   navigator.clipboard.writeText(copiarTexto).then(() => {
